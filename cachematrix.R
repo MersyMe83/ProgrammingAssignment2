7e610bfd9329e738c88829ee8fa84b894feaedad
MersyMe83 <65384970+MersyMe83@users.noreply.github.com>
2020-06-07 00:32:22 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## MersyMe83
+## Week 3: Assignment 2: Lexical Scoping
+## June 6, 2020
 
-## Write a short comment describing this function
+## Assignment Instructions: Catch the Inverse of a Matrix
+
+## The goal of this assignment is to write a pair of functions 
+## that cache the value of a matrix so it can be looked up in the cache rather than computed repeatedly. 
+
+## Create a function to compute the inverse of the special matrix returned
+## by the makeCacheMatric function.The function will set the variable inv to NULL,
+## return the matrix, store the calculated inverse, retrieve the cached inverse
+## and return the special matrix when called.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## Create a function that retrieves the inverse of the special matrix
+## assuming the value has been cached 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data,...)
+  x$setInverse(inv)
+  inv
 }
